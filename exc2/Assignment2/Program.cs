d9b95d0244d1a2208e546c59e31a93f1ed1d2003
AntonA <anton.akov@gmail.com>
2017-02-23 11:15:13 +0100

    exc2Old

---
+++ exc2/Assignment2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DNP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("2+3=" + MathLib.Sum(2, 3));
+            Console.WriteLine("2-3=" + MathLib.Sub(2, 3));
+            Console.WriteLine("2*3=" + MathLib.Mul(2, 3));
+            Console.WriteLine("2/3=" + MathLib.Div(2, 3));
+
+            //ideally every time you wanted to devide you'd run a piece of code with the surround/catch block
+            Console.Write("2/0=");
+            try { Console.Write(MathLib.Div(2, 0)); } catch (DivideByZeroException) { Console.WriteLine("Cannot divide by 0"); }
+
+            Console.WriteLine("2.1+3.3=" + MathLib.Sum(2.1, 3.3));
+            Console.WriteLine("2.2-3.3=" + MathLib.Sub(2.2, 3.3));
+            Console.WriteLine("2.4*3=" + MathLib.Mul(2.4, 3));
+            Console.WriteLine("2.4/3=" + MathLib.Div(2.4, 3));
+
+            Console.Write("2.5/0=");
+            try { Console.Write(MathLib.Div(2.5, 0)); } catch (DivideByZeroException) { Console.Write("Cannot divide by 0"); }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ exc2/MathLib/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNP
+{
+    public class MathLib
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Div(int a, int b)
+        {
+            if (b == 0) throw new DivideByZeroException("You cannot devide by zero");
+            else return a / b;
+        }
+
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        public static double Sub(double a, double b)
+        {
+            return a - b;
+        }
+        public static double Mul(double a, double b)
+        {
+            return a * b;
+        }
+        public static double Div(double a, double b)
+        {
+            if (b == 0) throw new DivideByZeroException("You cannot devide by zero");
+            else return a / b;
+        }
+    }
+}
