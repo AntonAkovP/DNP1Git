48b7a6bbec2601e0a47403b050d47cab76ed10c6
AntonA <anton.akov@gmail.com>
2017-03-02 11:50:43 +0100

    basic server client started, not finished

---
+++ exc 9,10,11(baseclasslib)/exc11Client/startC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exc11Client
+{
+    class startC
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("127.0.0.1", 12345);
+            NetworkStream networkStream = client.GetStream();
+            byte[] abyString = Encoding.ASCII.GetBytes("HEJ");
+            networkStream.Write(abyString, 0, 3);
+            Console.WriteLine(networkStream.Read());
+        }
+    }
+}
---
+++ exc 9,10,11(baseclasslib)/exc11Server/startS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exc11Server
+{
+    class startS
+    {
+        static void Main(string[] args)
+        {
+            byte[] data;
+            byte[] adr = { 127, 0, 0, 1 };
+            IPAddress ipAdr = new IPAddress(adr);
+            TcpListener newsock = new TcpListener(ipAdr, 12345);
+            newsock.Start();
+            Console.WriteLine("Waiting for a client...");
+            TcpClient client = newsock.AcceptTcpClient();
+            NetworkStream ns = client.GetStream();
+            string welcome = "Welcome to the DNPI1 test server";
+            data = Encoding.ASCII.GetBytes(welcome);
+            ns.Write(data, 0, data.Length);
+        }
+    }
+}
