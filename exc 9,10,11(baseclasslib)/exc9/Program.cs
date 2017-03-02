70a694c80ba435d404b161cd92eaebb27fce57e8
AntonA <anton.akov@gmail.com>
2017-03-02 10:21:39 +0100

    exc9

---
+++ exc 9,10,11(baseclasslib)/exc9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace exc9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asdf\n
+            Reader txt1Reader = new Reader(@"D:\shoolshit\DNP1Git\exc 9,10,11(baseclasslib)\exc9\txt1.txt");
+            //asdf\n
+            Reader txt2Reader = new Reader(@"D:\shoolshit\DNP1Git\exc 9,10,11(baseclasslib)\exc9\txt2.txt");
+
+            Thread t1 = new Thread(txt1Reader.Read);
+            Thread t2 = new Thread(txt2Reader.Read);
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine(txt1Reader.data.CompareTo(txt2Reader.data));
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ exc 9,10,11(baseclasslib)/exc9/Reader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+
+
+namespace exc9
+{
+    class Reader
+    {
+        string fileName;
+        public string data;
+
+        public Reader(string fn) { fileName = fn; }
+
+        public void Read()
+        {
+            FileStream s = new FileStream(fileName, FileMode.Open);
+            StreamReader r = new StreamReader(s);
+            data = r.ReadToEnd();
+            r.Close();
+            s.Close();
+        }
+    }
+}
