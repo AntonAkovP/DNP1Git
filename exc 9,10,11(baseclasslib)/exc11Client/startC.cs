c490ed779524378564a26016f1980a0d2e000810
AntonA <anton.akov@gmail.com>
2017-03-03 12:17:11 +0100

    server/client finished

--- exc 9,10,11(baseclasslib)/exc11Client/startC.cs
+++ exc 9,10,11(baseclasslib)/exc11Client/startC.cs
@@ -13,10 +13,36 @@ namespace exc11Client
         static void Main(string[] args)
         {
             TcpClient client = new TcpClient("127.0.0.1", 12345);
-            NetworkStream networkStream = client.GetStream();
-            byte[] abyString = Encoding.ASCII.GetBytes("HEJ");
-            networkStream.Write(abyString, 0, 3);
-            Console.WriteLine(networkStream.Read());
+            NetworkStream ns = client.GetStream();
+
+
+            byte[] buff = new byte[256];
+            int bread;
+            byte[] writeBuff;
+            StringBuilder message = new StringBuilder("Cannot read from server");
+
+            while (true)
+            {
+                if (ns.CanWrite)
+                {
+                    writeBuff = Encoding.ASCII.GetBytes(Console.ReadLine());
+                    ns.Write(writeBuff, 0, writeBuff.Length);
+                }
+                if (ns.CanRead)
+                {
+                    buff = new byte[128];
+                    message = new StringBuilder();
+
+                    do
+                    {
+                        bread = ns.Read(buff, 0, buff.Length);
+                        message.AppendFormat("{0}", Encoding.ASCII.GetString(buff, 0, bread));
+                    }
+                    while (ns.DataAvailable);
+
+                    Console.WriteLine(message);
+                }
+            }
         }
     }
 }
--- exc 9,10,11(baseclasslib)/exc11Server/startS.cs
+++ exc 9,10,11(baseclasslib)/exc11Server/startS.cs
@@ -4,25 +4,83 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace exc11Server
 {
     class startS
     {
-        static void Main(string[] args)
+
+
+        private static void ListenerM()
         {
-            byte[] data;
             byte[] adr = { 127, 0, 0, 1 };
             IPAddress ipAdr = new IPAddress(adr);
             TcpListener newsock = new TcpListener(ipAdr, 12345);
             newsock.Start();
-            Console.WriteLine("Waiting for a client...");
-            TcpClient client = newsock.AcceptTcpClient();
+            while(true)
+            {
+                Console.WriteLine("Waiting for a client...");
+                TcpClient client = newsock.AcceptTcpClient();
+                Thread handler = new Thread(handlerM);
+                handler.Start(client);
+
+                Console.WriteLine(client.ToString() + " has connected.");
+            }
+            
+        }
+
+        private static void handlerM(object clientO)
+        {
+            TcpClient client = clientO as TcpClient;
+            if (client==null) return;
+
+
             NetworkStream ns = client.GetStream();
-            string welcome = "Welcome to the DNPI1 test server";
-            data = Encoding.ASCII.GetBytes(welcome);
-            ns.Write(data, 0, data.Length);
+
+            byte[] buff;
+            byte[] writeBuff;
+            StringBuilder message = new StringBuilder("Message not read");
+            int bread;
+
+            while (true)
+            {
+                try
+                {
+
+
+                    if (ns.CanRead)
+                    {
+                        buff = new byte[128];
+                        message = new StringBuilder();
+
+                        do
+                        {
+                            bread = ns.Read(buff, 0, buff.Length);
+
+                            message.AppendFormat("{0}", Encoding.ASCII.GetString(buff, 0, bread));
+
+                        }
+                        while (ns.DataAvailable);
+                    }
+                    if (ns.CanWrite)
+                    {
+                        writeBuff = Encoding.ASCII.GetBytes("ECHO: " + message);
+                        ns.Write(writeBuff, 0, writeBuff.Length);
+                    }
+                }
+                catch (System.IO.IOException e) { Console.WriteLine(client.ToString() + " disconnected."); break; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread listener = new Thread(ListenerM);
+            listener.IsBackground = true;
+            listener.Start();
+
+            Console.ReadKey();
+
         }
     }
 }
