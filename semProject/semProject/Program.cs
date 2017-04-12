42dfa0f280fa6c57389b452591b7c70bfa539079
AntonA <anton.akov@gmail.com>
2017-04-12 15:55:22 +0200

    commit2

--- semProject/semProject/Program.cs
+++ semProject/semProject/Program.cs
@@ -8,9 +8,7 @@ namespace semProject
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
