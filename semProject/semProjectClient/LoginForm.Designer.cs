1e44db12b03ec1ccbb6937403470466621702ce2
AntonA <anton.akov@gmail.com>
2017-04-28 15:03:47 +0200

    commented and tested

--- semProject/semProjectClient/LoginForm.Designer.cs
+++ semProject/semProjectClient/LoginForm.Designer.cs
@@ -16,6 +16,7 @@
             if (disposing && (components != null))
             {
                 components.Dispose();
+                client.Close();
             }
             base.Dispose(disposing);
         }
